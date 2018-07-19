3dc8d8896a391cb686481c39b64d01aab7a4caaa
Kitavi <you@example.com>
2018-07-19 21:40:08 +0300

    static_files

--- LMS/settings.py
+++ LMS/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Courses',
+    'material',
+    'material.frontend'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS  = [
+    os.path.join(BASE_DIR, "static"),
+
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+ 
--- LMS/urls.py
+++ LMS/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include,url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 
@@ -21,3 +23,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^Courses/', include("Courses.urls", namespace="Courses")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
