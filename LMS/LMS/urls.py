a38d1d7ab9d1de5b1f6f09fc0f9345b9f8f3aa09
Steven Kitavi <steven.kitavi@gmail.com>
2025-07-16 17:30:30 +0300

    Refactor LMS, Put it back to development (#6)

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import include,url
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 
-
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^Courses/', include("Courses.urls", namespace="Courses")),
+    path('admin/', admin.site.urls),
+    path('Courses/', include("Courses.urls", namespace="Courses")),
 ]
 
 if settings.DEBUG:
